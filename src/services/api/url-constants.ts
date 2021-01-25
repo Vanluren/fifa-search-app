6c6f8ba45bf3455eb248e588b26ce5ce2164ef9e
Villads Nielsen <villadsvalur@gmail.com>
2021-01-25 15:50:24 +0100

    update to use the api suffix in the requests

--- src/services/api/url-constants.ts
+++ src/services/api/url-constants.ts
@@ -1,7 +1,7 @@
 const BASE_URL =
   process.env.NODE_ENV === "development"
-    ? "//localhost:3000"
-    : "//fifa.vanluren.xyz";
+    ? "//localhost:3000/api/v1"
+    : "//fifa.vanluren.xyz/api/";
 
 export const URL_STRINGS = {
   BASE_URL,
