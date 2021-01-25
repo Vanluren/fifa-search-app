1a31e06f73c3c50ca6fd07a2351f0a820846d546
Villads Nielsen <villadsvalur@gmail.com>
2021-01-25 15:08:08 +0100

    finished the player card

--- src/components/molecules/PlayerCard/index.tsx
+++ src/components/molecules/PlayerCard/index.tsx
@@ -6,13 +6,23 @@ import { Player } from "types";
 
 const PlayerCard = ({ name, image, position, value, club }: Player) => {
   return (
-    <div className="w-2/6 mx-4">
+    <div className="w-2/6 h-56 mx-4">
       <Card>
         <div className="flex flex-row">
           <Mugshot imageSrc={image} name={name} />
           <div className="flex flex-col ml-2">
             <Header size="sm">{club}</Header>
             <Header>{name}</Header>
+          </div>
+        </div>
+        <div className="flex flex-col px-2 py-6 w-full">
+          <div className="flex flex-row justify-between w-full">
+            <Header size="sm">Position</Header>
+            <Header size="md">{position}</Header>
+          </div>
+          <div className="flex flex-row justify-between w-full">
+            <Header size="sm">Market Value</Header>
+            <Header size="md">{value}</Header>
           </div>
         </div>
       </Card>
